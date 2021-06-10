48dd0035331350ab024d62352b383423ed40c976
Niklas Sandén <sandenniklas@gmail.com>
2021-06-10 21:13:54 +0200

    5.7 Updated Results

--- Part-I/5. Monte Carlo Methods/5.7 Racetrack MC Control/main.py
+++ Part-I/5. Monte Carlo Methods/5.7 Racetrack MC Control/main.py
@@ -43,25 +43,25 @@ class GreedyAgent(object):
         self.Q = Q
 
 # PARAMETERS - behaviour is ignored and target is used as agent when OFF_POLICY = False
-EPISODES = 2000000
+EPISODES = 20000
 MAX_Y_SPEED, MAX_X_SPEED = (4, 4)
 ENVIRONMENT = Racetrack_1(MAX_Y_SPEED, MAX_X_SPEED)
-TARGET = EpsilonGreedyAgent(ENVIRONMENT, epsilon=0.1)
-BEHAVIOUR = None
+TARGET = GreedyAgent(ENVIRONMENT)
+BEHAVIOUR = EpsilonGreedyAgent(ENVIRONMENT, epsilon=0.1)
 GAMMA = 1.0
 NUM_START_STATES = 6
-OFF_POLICY = False
+OFF_POLICY = True
 
 if __name__ == '__main__':
     if OFF_POLICY:
-        monte_carlo_q_off_policy_control(ENVIRONMENT, TARGET, BEHAVIOUR, GAMMA, max_iterations=EPISODES)
+        # Having pessimistic Q-values is essential for off-policy and is preferred for on-policy as well
+        monte_carlo_q_off_policy_control(ENVIRONMENT, TARGET, BEHAVIOUR, GAMMA, max_iterations=EPISODES, target_Q=defaultdict(lambda: -1000000), behaviour_Q=defaultdict(lambda: -1000000))
     else:
-        monte_carlo_q_on_policy_control(ENVIRONMENT, TARGET, GAMMA, max_iterations=EPISODES)
+        monte_carlo_q_on_policy_control(ENVIRONMENT, TARGET, GAMMA, max_iterations=EPISODES, start_Q=defaultdict(lambda: -1000000))
 
     tested_states = set()
     # If using off-policy with a deterministic target policy - this could loop indefinitely if it hasn't trained "long enough".
-    # 2 million episodes is not enough. To showcase the results of off-policy, another way to represent the policy is needed
-    # or add some randomness to move out of cycles.
+    # Perhaps add some randomness or check if there is a cycle to try to avoid it.
     while len(tested_states) < NUM_START_STATES: 
         state = ENVIRONMENT.reset()
         if state in tested_states:
@@ -75,6 +75,7 @@ if __name__ == '__main__':
             iterator += 1
             track[(state[0], state[1])] = iterator
             action = TARGET(state)
+            # step_after_noise avoids the randomness of the environment (check its source)
             state, reward, terminal, _ = ENVIRONMENT.step_after_noise(action)
         
         plt.imshow(track, interpolation='none')
